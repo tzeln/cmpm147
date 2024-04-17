325e0a5aa62e00a6543349d5e170e13721853b3f
tzeln <107380713+tzeln@users.noreply.github.com>
2024-04-16 22:53:56 -0700

    adding exp2 so far

--- experiment2/js/sketch.js
+++ experiment2/js/sketch.js
@@ -1,79 +1,126 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
-
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
-
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
-
-// Globals
-let myInstance;
-let canvasContainer;
-var centerHorz, centerVert;
-
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
-}
-
 function resizeScreen() {
   centerHorz = canvasContainer.width() / 2; // Adjusted for drawing logic
   centerVert = canvasContainer.height() / 2; // Adjusted for drawing logic
   console.log("Resizing...");
   resizeCanvas(canvasContainer.width(), canvasContainer.height());
-  // redrawCanvas(); // Redraw everything based on new size
 }
 
-// setup() function is called once when the program starts
+
 function setup() {
-  // place our canvas, making it fit our container
   canvasContainer = $("#canvas-container");
   let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
   canvas.parent("canvas-container");
-  // resize canvas is the page is resized
-
-  // create an instance of the class
-  myInstance = new MyClass("VALUE1", "VALUE2");
-
   $(window).resize(function() {
     resizeScreen();
   });
   resizeScreen();
+  createButton("reimagine").mousePressed(() => seed++);
 }
 
-// draw() function is called repeatedly, it's the main animation loop
+let seed = 239;
+
+const skyAmbience1 = "#E8F4FF";
+const skyAmbience2 = "#BEDBF9";
+const hills1 = "#89B0D9";
+const hills2 = "#7DA3CF";
+const hills3 = "#57606F";
+const hills4 = "#3E526B";
+const canyon1 = "#AA676D";
+const canyon2 = "#4C3841";
+const treeColor = "#5F6E77"
+
 function draw() {
-  background(220);    
-  // call a method on the instance
-  myInstance.myMethod();
-
-  // Set up rotation for the rectangle
-  push(); // Save the current drawing context
-  translate(centerHorz, centerVert); // Move the origin to the rectangle's center
-  rotate(frameCount / 100.0); // Rotate by frameCount to animate the rotation
-  fill(234, 31, 81);
+  randomSeed(seed);
+  background(100);
   noStroke();
-  rect(-125, -125, 250, 250); // Draw the rectangle centered on the new origin
-  pop(); // Restore the original drawing context
+  
+  fill(skyAmbience1);
+  rect(0, 0, width, height);
 
-  // The text is not affected by the translate and rotate
-  fill(255);
-  textStyle(BOLD);
-  textSize(140);
-  text("p5*", centerHorz - 105, centerVert + 40);
+  fill(skyAmbience2);
+  const scrub = mouseX/width;
+  let z = random();
+  let x = width * ((random() + (scrub + millis() / 500000.0) / z) % 1);
+  let y = height + z;
+  beginShape();
+  curveVertex(-1000, y);
+  curveVertex(0, y * 0.2);
+  curveVertex(x , y * 0.4);
+  curveVertex(x+2000, y * 0.3);
+  endShape(CLOSE);
+
+  // from background to foreground; colors meant to match ambient perspective
+  fill(hills1);
+  beginShape();
+  curveVertex(-700, height);
+  curveVertex(-700, height);
+  curveVertex(width * 0.1, height * 0.3);
+  curveVertex(width * 0.4, height * 0.4);
+  curveVertex(width * 0.7, height * 0.8);
+  curveVertex(width, height);
+  endShape(CLOSE);
+  
+  fill(hills2);
+  beginShape();
+  curveVertex(width/3, height);
+  curveVertex(width/3, height);
+  curveVertex(width * 0.5, height * 0.6);
+  curveVertex(width * 0.7, height * 0.4);
+  curveVertex(width * 0.9, height * 0.3);
+  curveVertex(width+700, height);
+  endShape(CLOSE);
+
+  fill(hills3);
+  beginShape();
+  curveVertex(-700, height);
+  curveVertex(-700, height);
+  curveVertex(width * 0.2, height * 0.8);
+  curveVertex(width * 0.4, height * 0.9);
+  curveVertex(width/2, height+100);
+  endShape(CLOSE);
+ 
+  fill(hills4);
+  beginShape();
+  curveVertex(width * 0.6, height);
+  curveVertex(width * 0.6, height);
+  curveVertex(width * 0.8, height * 0.8);
+  curveVertex(width+700, height);
+  endShape(CLOSE);
+
+  fill(canyon1);
+  beginShape();
+  vertex(0, height);
+  vertex(0, height * 0.45);
+  vertex(0, height * 0.5);
+  vertex(width * 0.1, height * 0.45);
+  curveVertex(width * 0.4, height * 0.45);
+  curveVertex(width * 0.45, height * 0.7);
+  curveVertex(width * 0.7, height+100);
+  endShape(CLOSE);
+
+  // fill(canyon2);
+
+  beginShape();
+  vertex(0, height * 0.7);
+  const steps = 100;
+  for (let i = 0; i < steps + 1; i++) {
+    let x = ((width * i) / steps) % (width * 0.4);
+    let y =
+      height * 0.6 - (random() * random() * random() * height) / 3 - height / 40;
+    vertex(x, y);
+  }
+  vertex(width * 0.4, height * 0.5);
+  endShape(CLOSE);
+
+  fill(treeColor);
+  // const trees = 20*random();
+  // const scrub = mouseX/width;
+  // for (let i = 0; i < trees; i++) {
+  //   let z = random();
+  //   let x = width * ((random() + (scrub/50 + millis() / 500000.0) / z) % 1);
+  //   let s = width / 50 / z;
+  //   let y = height / 2 + height / 20 / z;
+  //   triangle(x, y - s, x - s / 4, y, x + s / 4, y);
+  // }
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
-}
